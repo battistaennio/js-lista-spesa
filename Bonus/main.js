d61c6815934cf5d3fb5e69cd1c0a1bd54694b4f8
battistaennio <ennio.battista99@gmail.com>
2024-06-11 18:53:45 +0200

    Bonus: add style e function add article

---
+++ Bonus/main.js
@@ -0,0 +1,41 @@
+//Lista della spesa con ciclo while
+
+//creo array lista spesa
+const spesa = ["pane", "latte", "uova", "farina", "sale", "olio"];
+
+//seleziono ul in pagina
+const ulLista = document.querySelector("ul");
+
+//indice per ciclo while
+let indice = 0;
+
+//ciclo while
+while (indice < spesa.length) {
+
+    //creo i li da inserire in ul
+    let aggiungoLi = `<li>${spesa[indice]}</li>`;
+
+    //incremento ad ogni cilo il valore di indice
+    indice++;
+
+    //stampo i li in ul
+    ulLista.innerHTML += aggiungoLi;
+}
+
+
+//seleziono button
+const addElement = document.querySelector("button");
+
+addElement.addEventListener("click",
+    function () {
+
+        //seleziono input
+        let userElement = document.getElementById("user-element").value;
+
+        //creo i li da inserire in ul
+        aggiungoLi = `<li>${userElement}</li>`;
+        
+        //stampo i li in ul
+        ulLista.innerHTML += aggiungoLi;
+    }
+)
