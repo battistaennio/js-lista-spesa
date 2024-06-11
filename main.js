632163360c5a69803437a2cf524a80c96c4b2697
battistaennio <ennio.battista99@gmail.com>
2024-06-11 16:52:39 +0200

    feat: create ul selector in js and cycle while index

--- main.js
+++ main.js
@@ -2,3 +2,10 @@
 
 //creo array lista spesa
 const spesa = ["pane", "latte", "uova", "farina", "sale", "olio"];
+
+//seleziono ul in pagina
+const ulLista = document.querySelector("ul");
+
+//indice per ciclo while
+let indice = 0;
+
